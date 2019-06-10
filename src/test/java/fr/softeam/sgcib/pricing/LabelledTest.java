bde6efa1443192357516364cd4d5a9b480ecf017
Sabeur BOUZIDI <bouzidi.saber@gmail.com>
2019-06-10 21:13:33 +0200

    Clean code

--- src/test/java/fr/softeam/sgcib/pricing/LabelledTest.java
+++ src/test/java/fr/softeam/sgcib/pricing/LabelledTest.java
@@ -11,8 +11,6 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
-
 public class LabelledTest {
 
     @Test
--- src/test/java/fr/softeam/sgcib/pricing/discount/DiscountStrategyTest.java
+++ src/test/java/fr/softeam/sgcib/pricing/discount/DiscountStrategyTest.java
@@ -1,13 +1,9 @@
 package fr.softeam.sgcib.pricing.discount;
 
-import fr.softeam.sgcib.pricing.strategies.DefaultPricingStrategy;
-import fr.softeam.sgcib.pricing.strategies.PricingStrategy;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-
-import static org.junit.Assert.*;
 
 public class DiscountStrategyTest {
 
